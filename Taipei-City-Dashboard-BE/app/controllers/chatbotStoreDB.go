bf339bec44d39e484b59181e58a7fa02d505fef9
Kevin171717 <kevin47338647@gmail.com>
2024-07-06 13:51:10 +0800

    updata StoreDB

--- Taipei-City-Dashboard-BE/app/controllers/chatbotStoreDB.go
+++ Taipei-City-Dashboard-BE/app/controllers/chatbotStoreDB.go
@@ -1,10 +1,31 @@
 package controllers
+
 import (
+	"TaipeiCityDashboardBE/app/models"
 	"fmt"
+	"strings"
 )
+
 func StoreDB(repMsg repMessage) repMessage {
 	fmt.Println("-------StoreDB--------")
-	
-	// TODO store db
+	if strings.HasPrefix(repMsg.Message, "!a") {
+		repMsg.MessageType = "announcement"
+	}
+
+	if strings.HasPrefix(repMsg.Message, "!w") {
+		repMsg.MessageType = "wish"
+	}
+
+	if repMsg.MessageType == "announcement" {
+		// TODO store db
+		table := models.DBManager.Table("rep_message")
+		// store
+		err := table.Create(&repMsg).Error
+		if err != nil {
+			fmt.Println("Error when store rep_message into database")
+		}
+
+	}
+
 	return repMsg
 }
