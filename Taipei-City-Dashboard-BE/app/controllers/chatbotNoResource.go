0faed3cc1041845787f33729608b711a561e51cd
Kevin171717 <kevin47338647@gmail.com>
2024-07-06 21:25:33 +0800

    update NoResourceLocation

--- Taipei-City-Dashboard-BE/app/controllers/chatbotNoResource.go
+++ Taipei-City-Dashboard-BE/app/controllers/chatbotNoResource.go
@@ -2,19 +2,21 @@ package controllers
 
 import (
 	// "TaipeiCityDashboardBE/app/models"
+	"TaipeiCityDashboardBE/app/models"
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 type NoResourceLocation struct {
-	theType string `json:"theType"`
-	lat float64 `json:"lat"`
-	lng float64 `json:"lng"`
-	message string `json:"message"`
-
+	theType string    `json:"theType" gorm:"type"`
+	lat     float64   `json:"lat" gorm:"lat"`
+	lng     float64   `json:"lng" gorm:"lng"`
+	message string    `json:"message" gorm:"content"`
+	theTime time.Time `json:"theTime" gorm:"time"`
 }
 
 func StoreNoResourceLocation(theType string, lat float64, lng float64, message string) bool {
@@ -28,8 +30,21 @@ func StoreNoResourceLocation(theType string, lat float64, lng float64, message s
 
 	fmt.Println(happenTime)
 
-	// Todo store
+	// 创建一个 NoResourceLocation 实例并填充数据
+	report := NoResourceLocation{
+		theType: theType,
+		lat:     lat,
+		lng:     lng,
+		message: message,
+		theTime: happenTime,
+	}
 
+	// 使用 GORM 将数据插入到数据库中
+	result := models.DBDashboard.Table("report").Create(&report)
+	if result.Error != nil {
+		fmt.Println("Error inserting data:", result.Error)
+		return false
+	}
 
 	return true
 }
@@ -39,9 +54,9 @@ func NoResourceR(c *gin.Context) {
 	fmt.Println("-------NoResource RESTFUL--------")
 	// input as a json
 	// form: !no elec 21.1234 121.1234 here does not have eletricity
-	
+
 	var noResourceLocation NoResourceLocation
-	
+
 	if err := c.ShouldBindJSON(&noResourceLocation); err != nil {
 		c.JSON(200, gin.H{
 			"message": "error when store no resource message",
@@ -52,8 +67,6 @@ func NoResourceR(c *gin.Context) {
 	return
 }
 
-
-
 // for chatbot
 func NoResourceC(repMsg repMessage) repMessage {
 	fmt.Println("-------NoResource--------")
@@ -62,25 +75,25 @@ func NoResourceC(repMsg repMessage) repMessage {
 	theType := part[1]
 
 	lat, err := strconv.ParseFloat(part[2], 64)
-    if err != nil {
-        fmt.Println("转换错误:", err)
+	if err != nil {
+		fmt.Println("转换错误:", err)
 		repMsg.Message = "error when store no resource message"
 		return repMsg
-    }
+	}
 	lng, err := strconv.ParseFloat(part[3], 64)
-    if err != nil {
-        fmt.Println("转换错误:", err)
+	if err != nil {
+		fmt.Println("转换错误:", err)
 		repMsg.Message = "error when store no resource message"
 		return repMsg
-    }
+	}
 	message := part[4]
 	fmt.Println(message)
 	isSuccess := StoreNoResourceLocation(theType, lat, lng, message)
-	if isSuccess{
+	if isSuccess {
 		repMsg.Message = "have set as no resource message"
-	} else{
+	} else {
 		repMsg.Message = "error when store no resource message"
 	}
-	
+
 	return repMsg
 }
